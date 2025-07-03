f5a5200dfdba8d670bbbd18e34f9c7fbddedd949
Martin <martinvanschoor@gmail.com>
2025-07-03 16:58:34 +0200

    Everything up to feature extraction using WavLM working correctly, unable to test though, too computationally intensive

--- knn_vc.py
+++ knn_vc.py
@@ -16,6 +16,8 @@ class kNN_VC(torch.nn.Module):
         self.hifigan = hifigan.eval()
         self.k = k
         self.device = device
+        self.resampler = None  
+        self.sr_target = 16000
         
     @torch.inference_mode()
     def get_features(self, audio_fn):
@@ -24,14 +26,24 @@ class kNN_VC(torch.nn.Module):
         VAD can be applie if vad=True
         """
         # Retrieve audio and convert to correct device
-        source_audio, _ = torchaudio.load(audio_fn)
-        print(source_audio.shape)
-        #source_audio = source_audio.to(self.device)
+        source_audio, sr = torchaudio.load(audio_fn)
+        # Convert to mono if stereo
+        if source_audio.shape[0] > 1:
+            source_audio = source_audio.mean(dim=0, keepdim=True)
+        # Resample to 16kHz if needed
+        if sr != self.sr_target:
+            # Create resampler only once
+            if self.resampler is None or self.resampler.orig_freq != sr:
+                self.resampler = torchaudio.transforms.Resample(orig_freq=sr, new_freq=self.sr_target)
+            source_audio = self.resampler(source_audio)
+            sr = self.sr_target
+        source_audio = source_audio.to(self.device)
         
         # Use the WavLM SSL to extract features
-        #features, _ = self.wavlm.extract_features(source_audio, output_layer=6)
-        #features = features.squeeze()
-        #return features
+        features, _ = self.wavlm.extract_features(source_audio, output_layer=6)
+        features = features.squeeze(0)
+        
+        return features
     
     @torch.inference_mode()
     def vocode(self, input_features):
@@ -56,11 +68,10 @@ def main():
     device = "cpu"
     source_wav_filename = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/source1_martin.wav"
     target_wav_filename = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/target1_trump.wav"
-    output_path = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/"
+    output_path = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/output1.wav"
     # Load in the neccessary models (SSL feature extractor and Vocoder)
-    wavlm = torch.hub.load("bshall/knn-vc", "wavlm_large", trust_repo=True, device=device)
-    hifigan, _ = torch.hub.load("bshall/knn-vc", "hifigan_wavlm", trust_repo=True, device=device, prematched=True)
-    print("Models loaded in successfully")
+    #wavlm = torch.hub.load("bshall/knn-vc", "wavlm_large", trust_repo=True, device=device)
+    #hifigan, _ = torch.hub.load("bshall/knn-vc", "hifigan_wavlm", trust_repo=True, device=device, prematched=True)
 
 if __name__ == "__main__":
     main()
