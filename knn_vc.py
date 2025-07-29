5b486c5b16e247f580fa2e3d9b817e3a00cc3745
Martin <martinvanschoor@gmail.com>
2025-07-29 16:25:58 +0200

    writing target extraction code for librrispeech data

--- knn_vc.py
+++ knn_vc.py
@@ -7,7 +7,7 @@ import time
 import intelligibility
 from types import SimpleNamespace
 from pathlib import Path
-import tqdm
+from tqdm import tqdm
 
 n_frames = None
 k_top = 4
@@ -115,7 +115,8 @@ class kNN_VC(torch.nn.Module):
 def main():
     
 ### Specify filenames and other variables
-    device = "cuda"
+### For Laptop
+    device = "cpu"
     # Target 
     target_wav_filename = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/target/target3_obama.wav"
     target_feat_filename = "data/target/rfk2_5.pt"
@@ -128,6 +129,8 @@ def main():
     # Source and output, similarity
     eval_csv = Path("/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/eval.csv")
     output_dir_S = Path("/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/similarity")
+### For Desktop
+    # device = "cuda"
     
 ### Load in the neccessary models {SSL feature extractor (WavLM) and Vocoder (HiFi-GAN)}
     # wavlm = torch.hub.load("bshall/knn-vc", "wavlm_large", trust_repo=True, device=device)
--- target_extraction.py
+++ target_extraction.py
@@ -1,48 +1,51 @@
 import torch
 import torchaudio
 import torchaudio.functional as F
+from transformers import WavLMModel, WavLMPr
 import time
+from pathlib import Path
+from tqdm import tqdm
 
-def largest_divisor_in_range(n, low=5000, high=800_000):
+def largest_divisor_in_range(n, low=1, high=800_000):
     for d in range(high, low - 1, -1):
         if n % d == 0:
             return d
 
-target_fn = "/mnt/c/Users/marti/Tuts_Projects/Skripsie/Skripsie2025/data/target1_trump.wav"
-wavlm = torch.hub.load("bshall/knn-vc", "wavlm_large", trust_repo=True, device="cpu")
+# Define variables and import WavLM-large
+dev = "cuda"
+target_dir_og = Path("/mnt/c/Users/Martin/Documents/Werk/Universiteit/Skripsie_desktop/librispeech/LibriSpeech/test-clean")
+target_dir_new = Path("/mnt/c/Users/Martin/Documents/Werk/Universiteit/Skripsie_desktop/Skripsie2025_desktop/data/targets_librispeech/180")
+wavlm = torch.hub.load("bshall/knn-vc", "wavlm_large", trust_repo=True, device=dev)
 
-# Retrieve audio
-target_audio, sr = torchaudio.load(target_fn)
-print(target_audio.shape)
-# Convert to mono if stereo
-if target_audio.shape[0] > 1:
-    target_audio = target_audio.mean(dim=0, keepdim=True)
-print(target_audio.shape)
-# Resample to 16kHz if needed
-if not sr == 16000:
-    target_audio = F.resample(
-        target_audio,
-        orig_freq=sr,
-        new_freq=16000,
-    )
-# Convert to appropriate device
-target_audio = target_audio.to("cpu")
-print(target_audio.shape)
+# Loop through each speaker
+for speaker_dir in tqdm(sorted(target_dir_og.iterdir()), desc="Processing speakers"):
+    if not speaker_dir.is_dir():
+        continue
 
-start = time.time()
-# Divide the target audio into chunks, extract the features, append and store in a .npy file
-chunk_length = largest_divisor_in_range(target_audio.shape[1])
-print(f"Chunk length = {chunk_length}")
-chunk_list = []
-for i in range(target_audio.shape[1]//chunk_length):
-    chunk = target_audio[:,(i*chunk_length):((i+1)*chunk_length)]
-    with torch.inference_mode():
-        chunk_features, _ = wavlm.extract_features(chunk, output_layer=6)
-    chunk_features = chunk_features.squeeze()
-    chunk_list.append(chunk_features)
-target_features = torch.cat(chunk_list, dim=0)
-print(target_features.shape)
-end = time.time()
-print(f"Extraction took {end - start:.4f} seconds")
+    speaker_name = speaker_dir.name
+    audio = torch.empty(1, 0)  # shape: [1, 0]
+    print(speaker_name)
+
+# # Retrieve audio
+# target_audio, sr = torchaudio.load("/mnt/c/Users/Martin/Documents/Werk/Universiteit/Skripsie_desktop/librispeech/LibriSpeech/test-clean/1089/134686/1089-134686-0000.flac")
+# print(target_audio.shape)
+# # convert to appropriate device
+# target_audio = target_audio.to(dev)
+
+# start = time.time()
+# # Divide the target audio into chunks, extract the features, append and store in a .npy file
+# chunk_length = largest_divisor_in_range(target_audio.shape[1])
+# print(f"Chunk length = {chunk_length}")
+# chunk_list = []
+# for i in range(target_audio.shape[1]//chunk_length):
+#     chunk = target_audio[:,(i*chunk_length):((i+1)*chunk_length)]
+#     with torch.inference_mode():
+#         chunk_features, _ = wavlm.extract_features(chunk, output_layer=6)
+#     chunk_features = chunk_features.squeeze()
+#     chunk_list.append(chunk_features)
+# target_features = torch.cat(chunk_list, dim=0)
+# print(target_features.shape)
+# end = time.time()
+# print(f"Extraction took {end - start:.4f} seconds")
     
 
